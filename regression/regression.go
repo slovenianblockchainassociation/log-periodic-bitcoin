ad459ca011b1438158e9c3b769ade7bb51ac0d86
Peter Opara <opara.peter@gmail.com>
2018-03-09 09:52:53 +0100

    Removed helper function for sin.

--- regression/regression.go
+++ regression/regression.go
@@ -10,11 +10,6 @@ func cf(t, tc, omega, phi float64) float64 {
 	return math.Cos(omega*math.Log(tc-t) + phi)
 }
 
-// sin(Omega*ln(Tc - t) + Phi)
-func sf(t, tc, omega, phi float64) float64 {
-	return math.Sin(omega*math.Log(tc-t) + phi)
-}
-
 // f(t) = A + B*(Tc - t)^Beta * (1 + C*cos(Omega*ln(Tc - t) + Phi))
 func f(t float64, p *Parameters) float64 {
 	return p.A + p.B*math.Pow(p.Tc-t, p.Beta)*(1+p.C*cf(t, p.Tc, p.Omega, p.Phi))
